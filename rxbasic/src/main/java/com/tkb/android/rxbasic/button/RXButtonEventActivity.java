37c104958dc205d0286f56624ec5585303f48eb2
Tarun <tarun.pstu@gmail.com>
2019-04-03 18:00:13 +0600

    # RXAndroid Button click - added basic button action

--- rxbasic/src/main/java/com/tkb/android/rxbasic/button/RXButtonEventActivity.java
+++ rxbasic/src/main/java/com/tkb/android/rxbasic/button/RXButtonEventActivity.java
@@ -1,15 +1,13 @@
 package com.tkb.android.rxbasic.button;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
+import com.jakewharton.rxbinding2.view.RxView;
 import com.tkb.android.rxbasic.R;
 //https://code.tutsplus.com/tutorials/rxjava-for-android-apps-introducing-rxbinding-and-rxlifecycle--cms-28565
 public class RXButtonEventActivity extends AppCompatActivity {
@@ -20,6 +18,11 @@ public class RXButtonEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rxbutton_event);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button button = findViewById(R.id.btnOkay);
+
+        RxView.clicks(button)
+                .subscribe(aVoid -> Toast.makeText(RXButtonEventActivity.this,"Clicked",Toast.LENGTH_LONG).show());
 
 
     }
