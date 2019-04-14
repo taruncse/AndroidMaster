f05a5add355c62377fc0aa8fbc282795bb4c8772
Tarun <tarun.pstu@gmail.com>
2019-04-14 13:12:53 +0600

    - Added Login and registration design using ConstantLayout

---
+++ design/src/main/java/com/tkb/design/DesignMainActivity.java
@@ -0,0 +1,14 @@
+package com.tkb.design;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DesignMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design_main);
+    }
+}
