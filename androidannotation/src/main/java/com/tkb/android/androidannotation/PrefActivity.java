ac46bcbf1842abd0262f34d7115258b305516c50
Tarun <tarun.pstu@gmail.com>
2019-04-16 16:51:24 +0600

    # Android Annotation - store and view data from shared preference

--- androidannotation/src/main/java/com/tkb/android/androidannotation/PrefActivity.java
+++ androidannotation/src/main/java/com/tkb/android/androidannotation/PrefActivity.java
@@ -1,9 +1,16 @@
 package com.tkb.android.androidannotation;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 
 @EActivity(R.layout.activity_pref)
@@ -12,9 +19,27 @@ public class PrefActivity extends AppCompatActivity {
     @Pref
     PrefManager_ myPrefs;
 
+    @ViewById(R.id.txtShowName)
+    TextView txtShowName;
+
+    @ViewById(R.id.edtTextName)
+    EditText edtTextName;
+
+    @ViewById(R.id.btnSave)
+    Button btnSave;
+
+    @Click
+    void btnSave(){
+        if (edtTextName.getText().toString().isEmpty()){
+            Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
+        }else {
+            myPrefs.name().put(edtTextName.getText().toString());
+            txtShowName.setText(myPrefs.name().get()+"");
+
+        }
+    }
     @AfterViews
     protected void afterView() {
-        myPrefs.age().get();
-
+        txtShowName.setText(myPrefs.name().get()+"");
     }
 }
