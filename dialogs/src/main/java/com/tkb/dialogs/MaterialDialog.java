a2906254e4414b3ba8e28d812096f66fc1f86497
Tarun <tarun.pstu@gmail.com>
2019-04-28 20:48:55 +0600

    # Added dialogs module

---
+++ dialogs/src/main/java/com/tkb/dialogs/MaterialDialog.java
@@ -0,0 +1,14 @@
+package com.tkb.dialogs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MaterialDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_dialog);
+    }
+}
