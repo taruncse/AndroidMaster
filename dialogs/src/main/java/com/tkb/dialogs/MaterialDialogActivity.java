609bc34d9c09700a435fb210eac48cbbbe282225
Tarun <tarun.pstu@gmail.com>
2019-05-02 23:56:52 +0600

    - Dialog implementation

--- dialogs/src/main/java/com/tkb/dialogs/MaterialDialogActivity.java
+++ dialogs/src/main/java/com/tkb/dialogs/MaterialDialogActivity.java
@@ -1,12 +1,12 @@
 package com.tkb.dialogs;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.afollestad.materialdialogs.MaterialDialog;
+import com.yarolegovich.lovelydialog.LovelyInfoDialog;
+import com.yarolegovich.lovelydialog.LovelyTextInputDialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +20,8 @@ public class MaterialDialogActivity extends AppCompatActivity implements View.On
         initialization ();
 
         btnStandar.setOnClickListener(this);
+        btnInfo.setOnClickListener(this);
+        btnInput.setOnClickListener(this);
     }
 
     private void initialization() {
@@ -41,9 +43,24 @@ public class MaterialDialogActivity extends AppCompatActivity implements View.On
         }else if (v.getId() == R.id.btnCustom){
 
         }else if (v.getId() == R.id.btnInfo){
-
+            new LovelyInfoDialog(this)
+                    .setTopColorRes(R.color.mtrl_dark_on_primary_emphasis_high_type)
+                    .setIcon(R.drawable.ic_info_outline_white_24dp)
+                    //This will add Don't show again checkbox to the dialog. You can pass any ID as argument
+                    .setNotShowAgainOptionEnabled(0)
+                    .setNotShowAgainOptionChecked(true)
+                    .setTitle(R.string.info)
+                    .setMessage(R.string.app_name)
+                    .show();
         }else if (v.getId() == R.id.btnInput){
-
+            new LovelyTextInputDialog(this, R.style.TintTheme)
+                    .setTopColorRes(android.R.color.holo_orange_dark)
+                    .setTitle(R.string.text_input)
+                    .setMessage(R.string.toast_message)
+                    .setIcon(R.drawable.ic_info_outline_white_24dp)
+                    .setInputFilter(R.string.dont_show_again, text -> text.matches("\\w+"))
+                    .setConfirmButton(android.R.string.ok, text -> Toast.makeText(MaterialDialogActivity.this, text, Toast.LENGTH_SHORT).show())
+                    .show();
         }else if (v.getId() == R.id.btnProgress){
 
         }else {
